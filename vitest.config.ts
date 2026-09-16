d566b90efc17049d8523196881ee3447895707c9
CarloBarone-info <carlo.barone.info@gmail.com>
2026-09-16 23:17:37 +0200

    fix(test): paths for different spec tests need to be differentiated

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,6 +9,8 @@ export default mergeConfig(
       environment: "jsdom",
       setupFiles: ["./src/test/setup.ts"],
 
+      include: ["src/**/*.{test,spec}.{ts,tsx}"],
+
       coverage: {
         provider: "v8",
         reporter: ["text", "html"],
