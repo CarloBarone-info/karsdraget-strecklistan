066f53e73665a5cde72449c0f1fe4bc0a4662a79
CarloBarone-info <carlo.barone.info@gmail.com>
2026-09-16 22:58:19 +0200

    chore(deploy): GitHub pages deployment allowed through workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+// vite.config.ts
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base:
+    process.env.DEPLOY_TARGET === "github-pages"
+      ? "/karsdraget-strecklistan/"
+      : "/",
+});
